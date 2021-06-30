68ae386d5ad4958efb0dd6bbfacb89c6b8bdea05
Mike Bush <mike@survemobility.com>
2021-06-30 11:08:24 +0200

    Add support for cloning futures

--- src/future.rs
+++ src/future.rs
@@ -12,10 +12,16 @@ pub struct PromiseFuture<T> {
     shared: Arc<PromiseShared<T>>,
 }
 
-impl<T: Clone> Clone for PromiseFuture<T> {
+pub struct PromiseFutureMulti<T> {
+    shared: Arc<PromiseShared<T>>,
+    waker_index: Option<usize>,
+}
+
+impl<T: Clone> Clone for PromiseFutureMulti<T> {
     fn clone(&self) -> Self {
         Self {
             shared: self.shared.clone(),
+            waker_index: None,
         }
     }
 }
@@ -38,14 +44,14 @@ impl<T> Default for PromiseShared<T> {
 #[derive(Debug)]
 struct PromiseInner<T> {
     result: Option<T>,
-    waker: Option<Waker>,
+    wakers: Vec<Waker>, // the usual case is to have 0 or 1 wakers, more will only happen when merging. Ideally the first waker wouldn't require an allocation
 }
 
 impl<T> Default for PromiseInner<T> {
     fn default() -> Self {
         Self {
             result: None,
-            waker: None,
+            wakers: Vec::with_capacity(1),
         }
     }
 }
@@ -53,7 +59,7 @@ impl<T> Default for PromiseInner<T> {
 impl<T> Drop for Promise<T> {
     fn drop(&mut self) {
         self.shared.promise_dropped.store(true, Ordering::Release);
-        if let Some(waker) = self.shared.inner.lock().unwrap().waker.take() {
+        for waker in self.shared.inner.lock().unwrap().wakers.drain(..) {
             waker.wake();
         }
     }
@@ -76,6 +82,19 @@ impl<T> Promise<T> {
         )
     }
 
+    fn new_multi() -> (Promise<T>, PromiseFutureMulti<T>) {
+        let shared = Default::default();
+        (
+            Self {
+                shared: Arc::clone(&shared),
+            },
+            PromiseFutureMulti {
+                shared,
+                waker_index: None,
+            },
+        )
+    }
+
     fn set_value(self, result: T) {
         let mut data = self.shared.inner.lock().unwrap();
         // println!("Settign value on {:?}", (&data.result, &data.waker));
@@ -93,7 +112,39 @@ impl<T> Future for PromiseFuture<T> {
         } else if self.shared.promise_dropped.load(Ordering::Acquire) {
             Poll::Ready(Err(PromiseDropped))
         } else {
-            data.waker.replace(cx.waker().clone());
+            data.wakers.clear();
+            data.wakers.push(cx.waker().clone());
+            Poll::Pending
+        }
+    }
+}
+
+impl<T: Clone> Future for PromiseFutureMulti<T> {
+    type Output = Result<T, PromiseDropped>;
+
+    fn poll(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<Self::Output> {
+        let is_last = Arc::strong_count(&self.shared) == 1;
+        let mut data = self.shared.inner.lock().unwrap();
+        if let Some(result) = &data.result {
+            Poll::Ready(Ok(if is_last {
+                data.result.take().unwrap() // avoid unnecessary clone
+            } else {
+                result.clone()
+            }))
+        } else if self.shared.promise_dropped.load(Ordering::Acquire) {
+            Poll::Ready(Err(PromiseDropped))
+        } else {
+            if let Some(waker_idx) = self.waker_index {
+                data.wakers[waker_idx] = cx.waker().clone();
+            } else {
+                let waker_idx = data.wakers.len();
+                data.wakers.push(cx.waker().clone());
+                drop(data);
+                self.waker_index.replace(waker_idx);
+            }
             Poll::Pending
         }
     }
@@ -162,12 +213,74 @@ mod test {
     }
 
     #[test]
-    fn test_multi_with_result() {
-        let (promise, fut1) = Promise::new();
+    fn test_multi_with_multi_result() {
+        let (promise, fut1) = Promise::new_multi();
         let fut2 = fut1.clone();
         let job = MyJob(promise, "hello".to_string());
         thread::spawn(move || job.execute());
         assert_eq!(Ok("hello".to_string()), block_on(fut1));
         assert_eq!(Ok("hello".to_string()), block_on(fut2));
+    }
+
+    #[test]
+    fn test_multi_with_multi_result_already() {
+        let (promise, fut1) = Promise::new_multi();
+        let fut2 = fut1.clone();
+        let job = MyJob(promise, "hello".to_string());
+        job.execute();
+        assert_eq!(Ok("hello".to_string()), block_on(fut1));
+        assert_eq!(Ok("hello".to_string()), block_on(fut2));
+    }
+
+    #[test]
+    fn test_multi_with_multi_drop() {
+        let (promise, fut1) = Promise::new_multi();
+        let fut2 = fut1.clone();
+        let job = MyJob(promise, "hello".to_string());
+        thread::spawn(move || drop(job));
+        assert_eq!(Err(PromiseDropped), block_on(fut1));
+        assert_eq!(Err(PromiseDropped), block_on(fut2));
+    }
+
+    #[test]
+    fn test_multi_with_multi_drop_already() {
+        let (promise, fut1) = Promise::new_multi();
+        let fut2 = fut1.clone();
+        let job = MyJob(promise, "hello".to_string());
+        drop(job);
+        assert_eq!(Err(PromiseDropped), block_on(fut1));
+        assert_eq!(Err(PromiseDropped), block_on(fut2));
+    }
+
+    #[test]
+    fn test_multi_with_result() {
+        let (promise, fut) = Promise::new_multi();
+        let job = MyJob(promise, "hello".to_string());
+        thread::spawn(move || job.execute());
+        assert_eq!(Ok("hello".to_string()), block_on(fut));
+    }
+
+    #[test]
+    fn test_multi_with_result_already() {
+        let (promise, fut) = Promise::new_multi();
+        let job = MyJob(promise, "hello".to_string());
+        job.execute();
+        assert_eq!(Ok("hello".to_string()), block_on(fut));
+    }
+
+    #[test]
+    fn test_multi_with_drop() {
+        let (promise, fut) = Promise::new_multi();
+        let job = MyJob(promise, "hello".to_string());
+        thread::spawn(move || drop(job));
+        assert_eq!(Err(PromiseDropped), block_on(fut));
+    }
+
+    #[test]
+    fn test_multi_with_drop_already() {
+        let (promise, fut) = Promise::new_multi();
+        let job = MyJob(promise, "hello".to_string());
+        drop(job);
+        assert_eq!(Err(PromiseDropped), block_on(fut));
     }
 }
